cee06dd3665cac766a6b20a5866fc50f8aebe01e
vasiliev.artem <bacul@akado-ural.ru>
2023-04-29 17:59:32 +0500

    добавлена конфигурация webpack для production сборки

--- webpack.common.js
+++ webpack.common.js
@@ -2,20 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: ['./src/index.ts', './src/style.scss'],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist')
-        },
-        compress: true,
-        port: 3000
     },
     module: {
         rules: [
